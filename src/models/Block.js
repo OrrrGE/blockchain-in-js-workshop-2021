0c91e2449fd82f787f774ec342d991befbaec7d4
Tony Lemke <1585009480@qq.com>
2023-05-10 22:44:41 +0800

    Merge branch 'lesson2' into lesson1

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,9 +1,13 @@
 import sha256 from 'crypto-js/sha256.js'
+
+
+export const DIFFICULTY = 2
+
 class Block {
   // 1. 完成构造函数及其参数
-  /* 构造函数需要包含
 
-  */
+
+
   constructor(blockchain,previousHash, height, data) {
     this.previousHash=previousHash
     this.blockchain=blockchain
@@ -11,6 +15,29 @@ class Block {
     this.data=data
     this.hash=sha256(this.previousHash+this.blockchain+this.height+JSON.stringify(this.data)).toString()
   }
+
+  // isValid() 是用来检查区块是否合法的函数，需要完成以下内容：
+  isValid() {
+// 计算哈希值前缀0的个数
+    let prefix = 0;
+    for (let i = 0; i < this.hash.length; i++) {
+      if (this.hash[i] === '0') {
+        prefix++;
+      } else {
+        break;
+      }
+    }
+    // 比较前缀0的个数与难度值
+    return prefix >= DIFFICULTY;
+  }
+
+// setNonce(nonce) 是用来设置区块的 nonce 的函数，需要完成以下内容：
+  setNonce(nonce) {
+    this.nonce=nonce
+  }
+  
+
 }
 
 export default Block
+
