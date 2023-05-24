8039e2cc082fb90009cf5ad23d422e3ba1ffe40a
Tony Lemke <1585009480@qq.com>
2023-05-24 21:37:21 +0800

    coinTheory1

--- src/models/Block.js
+++ src/models/Block.js
@@ -1,25 +1,33 @@
-import sha256 from 'crypto-js/sha256.js'
+import sha256 from 'crypto-js/sha256.js';
+import MerkleTree from "./MerkleTree.js";
 import UTXO from "./UTXO.js";
 import UTXOPool from "./UTXOPool.js";
 
-
-export const DIFFICULTY = 2
+export const DIFFICULTY = 2;
 
 class Block {
-  // 1. 完成构造函数及其参数
-  constructor(blockchain,previousHash, height, data, miner) {
-    this.previousHash=previousHash
-    this.blockchain=blockchain
-    this.height=height
-    this.data=data
-    this.hash=sha256(this.previousHash+this.blockchain+this.height+JSON.stringify(this.data)+this.miner).toString()
-    this.utxoPool=new UTXOPool()
-    this.coinbaseBeneficiary=miner
+  constructor(blockchain, previousHash, height, data, miner) {
+    this.previousHash = previousHash;
+    this.blockchain = blockchain;
+    this.height = height;
+    this.merkleTree = new MerkleTree(data);
+    this.hash = this.calculateHash(miner);
+    this.utxoPool = new UTXOPool();
+    this.coinbaseBeneficiary = miner;
   }
 
-  // isValid() 是用来检查区块是否合法的函数，需要完成以下内容：
+  calculateHash(miner) {
+    return sha256(
+        this.previousHash +
+        this.blockchain +
+        this.height +
+        this.merkleTree.getRoot() +
+        miner
+    ).toString();
+  }
+
   isValid() {
-// 统计哈希值前缀0的个数
+    // 统计哈希值前缀0的个数
     let num = 0;
     for (let i = 0; i < this.hash.length; i++) {
       if (this.hash[i] === '0') {
@@ -32,18 +40,15 @@ class Block {
     return num >= DIFFICULTY;
   }
 
-// setNonce(nonce) 是用来设置区块的 nonce 的函数，需要完成以下内容：
   setNonce(nonce) {
-    this.hash=sha256(nonce+this.blockchain.name +
+    this.hash = sha256(
+        nonce +
+        this.blockchain.name +
         this.previousHash +
         this.height +
-        JSON.stringify(this.data)
+        this.merkleTree.getRoot()
     ).toString();
   }
-  
-
-
 }
 
-export default Block
-
+export default Block;
