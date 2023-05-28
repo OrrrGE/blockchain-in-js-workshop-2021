c1257be92aed40aa10ea6bfe7bd8ad0f73015107
Tony Lemke <1585009480@qq.com>
2023-05-28 18:45:08 +0800

    Update DictionaryTree.js

--- src/models/DictionaryTree.js
+++ src/models/DictionaryTree.js
@@ -14,25 +14,33 @@ class DictionaryTree{
     }
     // 插⼊⼀个单词
     insert(word){
+        // 从根节点开始
         let currentNode = this.root
+        // 遍历单词的每个字符
         for(let i = 0; i < word.length; i++){
         let currentChar = word[i]
+            // 如果当前节点没有这个字符，就添加⼀个节点
         if(!this.nodes[currentNode][currentChar]){
             this.nodes[currentNode][currentChar] = Object.keys(this.nodes).length
             this.nodes[this.nodes[currentNode][currentChar]] = {}
         }
+        // 继续遍历下⼀个节点
         currentNode = this.nodes[currentNode][currentChar]
         }
+        // 在最后⼀个节点添加结束标记
         this.nodes[currentNode]['-1'] = true
     }
     // 查找⼀个单词
     search(word){
         let currentNode = this.root
+        // 遍历单词的每个字符
         for(let i = 0; i < word.length; i++){
         let currentChar = word[i]
+            // 如果当前节点没有这个字符，就返回false
         if(!this.nodes[currentNode][currentChar]){
             return false
         }
+        // 继续遍历下⼀个节点
         currentNode = this.nodes[currentNode][currentChar]
         }
         return this.nodes[currentNode]['-1']
@@ -40,11 +48,14 @@ class DictionaryTree{
     // 查找前缀
     startsWith(prefix){
         let currentNode = this.root
+        // 遍历前缀的每个字符
         for(let i = 0; i < prefix.length; i++){
+            // 如果当前节点没有这个字符，就返回false
         let currentChar = prefix[i]
         if(!this.nodes[currentNode][currentChar]){
             return false
         }
+        // 继续遍历下⼀个节点
         currentNode = this.nodes[currentNode][currentChar]
         }
         return true
